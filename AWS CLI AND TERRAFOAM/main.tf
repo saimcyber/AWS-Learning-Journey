752c4055ea2db0982346759155b5cad9f1c83b85
Saim Zaib <saimzaib69@gmail.com>
2026-01-21 14:54:22 +0500

    Create lab2 S3 bucket with tags
    Added a new S3 bucket resource with tags for lab2.

--- AWS CLI AND TERRAFOAM/main.tf
+++ AWS CLI AND TERRAFOAM/main.tf
@@ -1,6 +1,13 @@
 provider "aws" {
   region = "us-east-1"
 }
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "saim-cloudsec-bucket-441290"
+
+resource "aws_s3_bucket" "lab2_bucket" {
+  bucket = "terraform-lab2-bucket-saimsec-12345"
+
+  tags = {
+  Name        = "Lab2Bucket"
+  Environment = "Dev"
+  Owner       = "Saim"
+    }
 }

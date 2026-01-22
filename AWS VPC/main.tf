8cb1ef22585edabf4da8bab8bfa6790b231f095a
Saim Zaib <saimzaib69@gmail.com>
2026-01-22 09:12:43 +0500

    feat: complete phase-2 VPC with public/private subnets, NAT, and bastion access

--- AWS VPC/main.tf
+++ AWS VPC/main.tf
@@ -37,6 +37,19 @@ resource "aws_route_table" "public_rt" {
     Name = "public-route-table"
   }
 }
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "private-route-table"
+  }
+}
+
 resource "aws_route_table_association" "public_assoc" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public_rt.id
@@ -70,3 +83,131 @@ resource "aws_instance" "public_ec2" {
     Name = "phase2-public-ec2"
   }
 }
+
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "private-subnet"
+  }
+}
+
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+}
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public.id
+
+  tags = {
+    Name = "phase2-nat-gateway"
+  }
+}
+resource "aws_route_table_association" "private_assoc" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private_rt.id
+}
+resource "aws_security_group" "web_sg" {
+  name   = "web-sg"
+  vpc_id = aws_vpc.main.id
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["172.17.5.55/32"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "private_sg" {
+  name   = "private-ec2-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.ssh_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "private-ec2-sg"
+  }
+}
+
+
+resource "aws_network_acl" "custom_nacl" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "custom-nacl"
+  }
+}
+
+resource "aws_network_acl_rule" "inbound_http" {
+  network_acl_id = aws_network_acl.custom_nacl.id
+  rule_number    = 100
+  egress         = false
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 80
+  to_port        = 80
+}
+
+resource "aws_network_acl_rule" "inbound_ssh" {
+  network_acl_id = aws_network_acl.custom_nacl.id
+  rule_number    = 110
+  egress         = false
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "172.17.5.55/32"
+  from_port      = 22
+  to_port        = 22
+}
+
+resource "aws_network_acl_rule" "outbound_all" {
+  network_acl_id = aws_network_acl.custom_nacl.id
+  rule_number    = 100
+  egress         = true
+  protocol       = "-1"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+}
+resource "aws_network_acl_association" "public_assoc" {
+  network_acl_id = aws_network_acl.custom_nacl.id
+  subnet_id      = aws_subnet.public.id
+}
+
+resource "aws_instance" "private_ec2" {
+  ami                    = "ami-0c02fb55956c7d316"
+  instance_type          = "t3.micro"
+  subnet_id              = aws_subnet.private.id
+  vpc_security_group_ids = [aws_security_group.private_sg.id]
+  key_name               = "first-key-pair"
+
+  tags = {
+    Name = "phase2-private-ec2"
+  }
+}
+
